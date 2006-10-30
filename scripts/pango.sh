07335cc125e46c438c7808a4c7f3cce0c325907a
Gregor <jxo@gregor.im>
2006-10-30 19:27:00 +0000

    Pango now includes pangocairo.
    git-svn-id: http://svn.dsource.org/projects/bcd/trunk/bcd.gen@112 519b2bcc-d214-0410-9f15-ecb23bf85076

--- scripts/pango.sh
+++ scripts/pango.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-export CFLAGS="$CFLAGS `pkg-config --cflags pango`"
+export CFLAGS="$CFLAGS `pkg-config --cflags pango` `pkg-config --cflags pangocairo`"
 
 rm -rf bcd/pango
 
 echo pango
 ./bcdgen $1/pango.h pango -C -A \
   -Fbcd.glib2.glib
+
+echo pangocairo
+./bcdgen $1/pangocairo.h pango -C \
+  -Fbcd.glib2.glib \
+  -Fbcd.cairo.cairo
+
